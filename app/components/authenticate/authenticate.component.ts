ae750865fd4071a38b86814c46fdd5a8cef90e4a
dragonprevost <dragon@dkp.io>
2019-05-24 19:29:43 -0700

    implented new functions for calling Oauth

--- app/components/authenticate/authenticate.component.ts
+++ app/components/authenticate/authenticate.component.ts
@@ -17,25 +17,8 @@ export class AuthenticateComponent implements OnInit {
     }
   }
 
-  switchToMainPanel(): void {
-    // Check if either the password/username or both fields are empty and show an appropriate message if they are
-    if (document.getElementById('password').value == "" && document.getElementById('username').value == "") {
-      emptyPassword();
-      emptyUsername();
-    } else if (document.getElementById('password').value == "") {
-      emptyPassword();
-      notEmptyUsername();
-    } else if (document.getElementById('username').value == "") {
-      emptyUsername();
-      notEmptyPassword();
-    } else {
-      // Both the fields filled so check if they can log in
-      notEmptyPassword();
-      notEmptyUsername();
-      
-      document.getElementById('grey-out').style.display = 'block';
-      getUserInfo(switchToAddRepositoryPanel);
-    }
+  authenticateGithub(): void {
+	authenticateUser(switchToAddRepositoryPanel);
   }
 
   createNewAccount(): void {
